740e5f3c281afe8b788dab061a7e98135c4e75b3
A-Kija <42836152+A-Kija@users.noreply.github.com>
2020-02-03 12:04:37 +0200

    masyvai ir rodykles

--- regex.php
+++ regex.php
@@ -3,14 +3,61 @@
 
 $str = 
 '2478sjdhfkjsd
-1678sdfsdfds
+2678sdfsdfds
 25014sdfsdf
-2478 jdhfkjsd
+2778 jdhfkjsd
+skjfhgkjsd 2999sdjkhfkjsdhfkja
 ';
 
-preg_match_all('/(2\d{3})(\D)/m', $str, $matches);
+preg_match_all('/(2\d{3})\D/m', $str, $matches);
 
-_dc($matches);
+// _d($matches);
+//  _dc($matches);
+
+// foreach($matches[1] as $key => $val) {
+//     $matches[1][$key] = 'AAA';
+// }
+
+
+foreach($matches[1] as &$val) {
+    $val = 'AAA';
+}
+
+// _dc($matches);
+
+$color = ['red', 'green', 'blue', 'orange'];
+
+foreach ($color as &$c) {
+    // _dc($c);
+}
+unset($c);
+echo "<br><br><br><br>";
+
+foreach ($color as $c) {
+    // _dc($color);
+    // _dc($c);
+}
+
+_dc($color);
+
+$masyvas = [
+    ['a','d'],
+    ['v','e'],
+    ['a','c'],
+    ['s','r'],
+];
+
+_dc($masyvas);
+
+usort($masyvas, function($jonas, $petras){
+    return -1*($jonas[0] <=> $petras[0]);
+});
+
+_dc($masyvas);
+
+
+
+
 
 
 
