1b1ff5dc041181cc68aea2e5bc4f9af00994113e
A-Kija <42836152+A-Kija@users.noreply.github.com>
2020-02-06 14:03:43 +0200

    ref per prieki

--- u/67.php
+++ u/67.php
@@ -18,15 +18,54 @@ function add($mas)
     return $m;
 }
 
-
-
-
-
-
 $masyvas = [];
 foreach(range(1, rand(2, 5)) as $val) {
 
     $masyvas = add($masyvas);
 }
 
+// _dc($masyvas);
+
+
+
+
+
+
+
+
+function rand_arr()
+{
+    $m = [];
+    foreach(range(0, rand(2, 3)) as $val) {
+        $m[] = rand(0, 10);
+    }
+    $m[count($m)-1] = 0;
+    return $m;
+}
+
+
+function re(&$arr)
+{
+    static $flag;
+    $flag = 1;
+    foreach($arr as $key => &$val) {
+        if (is_array($val)) {
+            re($val);
+        }
+    }
+    if (!$flag) {
+        return;
+    }
+    $flag = 0;
+    $val = rand_arr();
+}
+
+
+$masyvas = rand_arr();
+foreach(range(1, rand(5, 5)) as $val) {
+    re($masyvas);
+}
+
+
 _dc($masyvas);
+
